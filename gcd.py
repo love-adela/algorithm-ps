d04a3e11f821bf4fd9e4b530e8c033f5723b94a1
love-adelar <love.adelar@gmail.com>
2019-03-10 22:46:59 +0900

    최대공약수 구하기 : 코드 중복 해결, 계산 효율성 증대

--- gcd.py
+++ gcd.py
@@ -1,21 +1,9 @@
 import math
 
-
 def gcd(a, b):
-    if a >= b:
-        for i in range(1, a): # O(a)
-            if a % i == 0 and b % i == 0:
-                gcd = i
-        return gcd
-    if b > a:
-        for i in range(1, b): # O(b)
-            if a % i == 0 and b % i == 0:
-                gcd = i
-        return gcd
-
-# print(type(line.split()))
-# print(repr(line.split()))
-
+    for i in range(min(a, b), 0, -1):
+        if a % i == 0 and b % i == 0:
+            return i
 
 gcd_list = []
 while True:
