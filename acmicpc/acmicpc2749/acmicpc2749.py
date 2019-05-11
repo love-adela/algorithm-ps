bad75df2211dfae281dc1479d89616a2118454f1
love-adelar <love.adelar@gmail.com>
2019-05-11 20:00:05 +0900

    불필요한 파일 삭제

--- acmicpc/acmicpc2749/acmicpc2749.py
+++ acmicpc/acmicpc2749/acmicpc2749.py
@@ -52,3 +52,23 @@ def matrix_fibo(n:int) -> int:
 
     return get_nth(n)[1][0]
                     
+def fibonacci_matrix(n: int, mod: int) -> int:
+    arr, constant = [[1, 0], [0, 1]], [[1, 1], [1, 0]]
+    while n > 0:
+        if n % 2 == 1:
+            arr = multiply_matrix(arr, constant, mod)
+        constant = multiply_matrix(constant, constant, mod)
+        n = n // 2
+    return arr[0][0]
+
+def multiply_matrix(matrix_a, matrix_b, mod) -> list:
+    result = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                result[i][j] = (result[i][j] + matrix_a[i][k] * matrix_b[k][j]) % mod
+    return result
+
+mod = 10 ** 6
+print(fibonacci_matrix(int(input()) - 1, mod))
+
