59a8a99cf22f0a69c81c3e40aa670820aae3d8ad
love-adelar <love.adelar@gmail.com>
2019-06-02 03:20:36 +0900

    1260

---
+++ acmicpc/acmicpc1260/acmicpc1260.py
@@ -0,0 +1,34 @@
+def dfs(now, graph, visited)->list:
+    visited += [now]
+
+    for i in range(len(graph[now])):
+        if graph[now][i] and i not in visited:
+            visited = dfs(i, graph, visited)
+    return visited
+
+
+def bfs(start)->list:
+    queue = [start]
+    visited = [start]
+    while queue:
+        current = queue.pop(0)
+        for i in range(len(matrix[current])):
+            if matrix[current][i] and i not in visited:
+                visited += [i]
+                queue += [i]
+    return visited
+
+
+n, m, v = map(int, input().split())
+matrix = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    matrix[x][y] = 1
+    matrix[y][x] = 1
+    # link = list(map(int, input().split()))
+    # matrix[link[0]][link[1]] = 1
+    # matrix[link[1]][link[0]] = 1
+
+print(*dfs(v, matrix, []))
+print(*bfs(v))
+
