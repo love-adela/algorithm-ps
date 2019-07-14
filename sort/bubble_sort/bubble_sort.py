aa6cbafbc8d14faac2172e53ac7ccab77471e52f
love-adelar <love.adelar@gmail.com>
2019-07-14 19:37:12 +0900

    bubble sort 구현에 불필요한 코드 삭제

--- sort/bubble_sort/bubble_sort.py
+++ sort/bubble_sort/bubble_sort.py
@@ -1,8 +1,7 @@
-numbers = [int(param) for param in input().split(' ')]
+numbers = [int(param) for param in input().split()]
 
-# bubble sort
 for i in range(len(numbers)):
-    for iteration in range(len(numbers) - (1 + i)):
-        if numbers[iteration] > numbers[iteration + 1]:
-            numbers[iteration], numbers[iteration + 1] = numbers[iteration + 1], numbers[iteration]
+    for j in range(len(numbers) - (1+i)):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 print(' '.join(str(num) for num in numbers))
