d420c8aad52921e2eaa2612fc507ed46d79e64cd
love-adelar <love.adelar@gmail.com>
2019-07-14 20:08:45 +0900

    selection sort

--- sort/selection_sort/selection_sort.py
+++ sort/selection_sort/selection_sort.py
@@ -1,5 +1,3 @@
-# 5 12 23 34 45 54 56 234 345 3432
-# 23 34 234 5 12 345 3432 45 54 56
 numbers = [int(param) for param in input().split()]
 
 # selection sort - (1)
@@ -18,13 +16,3 @@ for i in range(len(numbers)):
             min_index = j
     numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
 
-# selection sort - (3)
-def selection_sort(my_list):
-    for i in range(len(my_list)):
-        for j in range(i, len(my_list)):
-            if my_list[i] > my_list[j]:
-                my_list[i], my_list[j] = my_list[j], my_list[i]
-
-some_list = [11, 3, 6, 4, 12, 1, 2]
-selection_sort(some_list)
-print(some_list)
