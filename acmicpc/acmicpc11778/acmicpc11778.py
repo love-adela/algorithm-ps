05094575ea21ad309b4387d23ec1da39ffac04ec
love-adelar <love.adelar@gmail.com>
2019-07-15 02:38:36 +0900

    11778번 제출

---
+++ acmicpc/acmicpc11778/acmicpc11778.py
@@ -0,0 +1,40 @@
+import sys
+
+read = lambda: map(int, sys.stdin.readline().split())
+p, q = read()
+
+def multiply_matrix(a, b):
+    MOD = 1_000_000_007
+    return (
+        (a[0]*b[0] + a[1]*b[2])%MOD, 
+        (a[0]*b[1] + a[1]*b[3])%MOD,
+        (a[2]*b[0] + a[3]*b[2])%MOD,
+        (a[2]*b[1] + a[3]*b[3])%MOD,
+    )
+
+def fibo_matrix(n):
+    if n == 0:
+        return (1, 0, 0, 1)
+    if n == 1:
+        return (1, 1, 1, 0)
+
+    a = fibo_matrix(n//2)
+    a_square = multiply_matrix(a, a)
+    if n % 2 == 0:
+        return a_square 
+    else:
+        return multiply_matrix(a_square, (1, 1, 1, 0))
+
+def fibo(n):
+    if n == 0:
+        return 0
+    return fibo_matrix((n-1)%2000000016)[0]
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+m = gcd(p, q)
+fibo = fibo(m)
+print(fibo)
