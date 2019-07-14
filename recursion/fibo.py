e5f7275857df5cab8c3a46dc4df3f603830726c1
love-adelar <love.adelar@gmail.com>
2019-07-14 22:29:08 +0900

    fibonacci 수열 행렬로 구현

---
+++ recursion/fibo.py
@@ -0,0 +1,27 @@
+# 행렬로 푸는 풀이: 최적해
+def fibo_matrix(n):
+    if n < 0:
+        print("정수를 입력하세요")
+    
+    current_item = 0
+    next_item = 1
+    for i in range(n):
+        (current_item, next_item) = (next_item, current_item + next_item)
+    return current_item
+
+print(fibo_matrix(7))
+
+# Memoization 하는 풀이 : O(n)
+def fibo(n):
+    # Base Case
+    if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    dict = {}
+    if n in dict:
+        return dict[n] # memoization
+    answer = fibo(n - 1) + fibo(n - 2)
+    dict[n] = answer
+    return answer
+
