4eb47a24e2007cd59b362ef9095ff74ecd2fd9c5
love-adelar <love.adelar@gmail.com>
2019-07-14 22:28:34 +0900

    file directory변경

--- recursion/recursive/euclidean/euclidean.py
+++
@@ -1,8 +0,0 @@
-m, n = map(int, input().split())
-
-def euclidean(m, n):
-    if n == 0:
-        return m
-    return euclidean(n, m % n)
-
-print(euclidean(m, n))
--- recursion/recursive/fibonacci/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def fib(n):
-    if n == 1 or n == 2:
-        return 1
-    return fib(n-1) + fib(n-2)
-
-
-for i in range(1, 11):
-    print(fib(i))
