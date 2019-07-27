b5408ed38052936625fb32eb953c829ad7f5e648
love-adelar <love.adelar@gmail.com>
2019-07-27 14:17:09 +0900

    주석 삭제

--- datastructure/hashtable/pearson_hash.py
+++ datastructure/hashtable/pearson_hash.py
@@ -1,12 +1,10 @@
 import random
 
-look_up_table = [x for x in range(256)] # 해쉬값을 쓰기 위해서 랜덤하게 ㅇㅇ를 생성
-# table = list(range(256))
+look_up_table = [x for x in range(256)]
 random.shuffle(table) # 고정시키려면 seed 사용할 것
 
 def pearson_hash(character:str, random_table:list) -> int:
-    hash = len(character) % len(random_table) # 256 (어떤 값을 넣어도 % 연산을 했을 때, 결과값이 0 ~ 255로 나온다.)
-
+    hash = len(character) % len(random_table) 
     for i in character:
         hash = random_table[hash^ord(i)]
     return hash
