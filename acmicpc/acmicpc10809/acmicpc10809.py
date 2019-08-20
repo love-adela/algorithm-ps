a646ec0060cf75d94be5de1fa42ff4e1ce614a20
love-adelar <love.adelar@gmail.com>
2019-08-20 09:51:52 +0900

    find()로 97~123 범위 내에서의 chr(i) 출력

--- acmicpc/acmicpc10809/acmicpc10809.py
+++ acmicpc/acmicpc10809/acmicpc10809.py
@@ -1,10 +1,2 @@
 import sys
-
-w = sys.stdin.readline().rstrip()
-alphabet_dic = {'a':-1, 'b':-1, 'c':-1, 'd':-1, 'e':-1, 'f':-1, 'g':-1, 'h':-1, 'i':-1, 'j':-1, 'k':-1, 'l':-1, 'm':-1, 'n':-1, 'o':-1, 'p':-1, 'q':-1, 'r':-1, 's':-1, 't':-1, 'u':-1, 'v':-1, 'w':-1, 'x':-1, 'y':-1, 'z':-1}
-for i in w:
-    alphabet_dic[i] = w.index(i)
-result = ''
-for _, value in alphabet_dic.items():
-    result += str(value) + ' '
-print(result)
+print(*map(sys.stdin.readline().rstrip().find, map(chr, range(97, 123))), sep=" ")
