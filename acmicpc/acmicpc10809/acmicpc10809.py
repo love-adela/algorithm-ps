29452fe928c24a081d0a7b3fe38e5205a4da6413
love-adelar <love.adelar@gmail.com>
2019-08-20 09:47:17 +0900

    dictionary 써서 해당 글자가 나올 때마다 -1을 index() 값으로 바꾸기.

--- acmicpc/acmicpc10809/acmicpc10809.py
+++ acmicpc/acmicpc10809/acmicpc10809.py
@@ -1,14 +1,10 @@
-string = input()
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+import sys
 
-def get_index(string, alphabet):
-    for i in range(len(string)):
-        for j in range(len(alphabet)):
-            if i == j:
-                return string[i]
-            else:
-                return -1
-
-
-print(get_index(string, alphabet))
-
+w = sys.stdin.readline().rstrip()
+alphabet_dic = {'a':-1, 'b':-1, 'c':-1, 'd':-1, 'e':-1, 'f':-1, 'g':-1, 'h':-1, 'i':-1, 'j':-1, 'k':-1, 'l':-1, 'm':-1, 'n':-1, 'o':-1, 'p':-1, 'q':-1, 'r':-1, 's':-1, 't':-1, 'u':-1, 'v':-1, 'w':-1, 'x':-1, 'y':-1, 'z':-1}
+for i in w:
+    alphabet_dic[i] = w.index(i)
+result = ''
+for _, value in alphabet_dic.items():
+    result += str(value) + ' '
+print(result)
