96ac9c97737d0cd85e44b23d2d86b4d1934ba5b4
love-adelar <love.adelar@gmail.com>
2019-09-20 00:28:11 +0900

    1018: if 문 대신 min() 사용. itertools.accumulate 사용

--- acmicpc/1018/1018.py
+++ acmicpc/1018/1018.py
@@ -1,25 +1,18 @@
 import sys
+from itertools import accumulate
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-
-b = [[0 for _ in range(m+1)] for _ in range(n+1)]
-
+n, m = map(int, input().split())
+f = lambda x: 1 if x == 'W' else 0
+b = [[0 for _ in range(m+1)]]
 for i in range(n):
-    status = sys.stdin.readline().strip()
-    for j in range(m):
-        temp = (i+j) % 2 
-        if status[j] == 'B':
-            temp = 1-temp
-        b[i+1][j+1] = b[i][j+1] + b[i+1][j] - b[i][j] + temp
+    cost = [0]
+    cost.extend(accumulate([(f(s)+i+j) % 2 for j, s in enumerate(input().rstrip())]))
+    b.append([k+j for k, j in zip(cost, b[-1])])
 
-max_value = 32
-
+result = 32
 for i in range(8, n+1):
     for j in range(8, m+1):
         x = b[i][j] + b[i-8][j-8] - b[i][j-8] - b[i-8][j]
-        if x > max_value:
-            x = 64-x
-        if x < max_value:
-            max_value = x
-print(max_value)
-
+        result = min(result, x, 64-x)
+print(result)
