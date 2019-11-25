a2193291e2c474df572c0cb6cc54569e8be96ab6
love-adelar <love.adelar@gmail.com>
2019-11-26 01:12:14 +0900

    좀 더 빠른 솔루션 찾음

--- acmicpc/2447/2447.py
+++ acmicpc/2447/2447.py
@@ -1,17 +1,12 @@
-import sys
-
-num = int(input())
-
-def get_star(i, j):
-    while i != 0:
-        if i % 3 == 1 and j % 3 == 1:
-            sys.stdout.write(' ')
-            return None
-        i=i//3
-        j=j//3
-    sys.stdout.write('*')
-
-for i in range(num):
-    for j in range(num):
-            get_star(i, j)
-    sys.stdout.write('\n')
+a=int(input())
+def s(n):
+    if n==3:return['***','* *','***']
+    x=s(n//3)
+    y=list(zip(x,x,x))
+    for i in range(len(y)):
+        y[i]=''.join(y[i])
+    z=list(zip(x,[' '*(n//3)]*(n//3),x))
+    for i in range(len(z)):
+        z[i]=''.join(z[i])
+    return y+z+y
+print('\n'.join(s(a)))
