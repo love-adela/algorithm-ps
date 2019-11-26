850ea2288126ab2f8a1c6e9043ae975250c0b784
love-adelar <love.adelar@gmail.com>
2019-11-27 01:56:09 +0900

    순회할 때 while문을 활용해 i**2이 이터레이팅 하도록 함. 속도 차이는 없음

--- acmicpc/2581/2581.py
+++ acmicpc/2581/2581.py
@@ -1,10 +1,11 @@
 def is_prime(n:int)->bool:
-    if n != 1:
-        for i in range(2, int(n**0.5)+1):
-            if n % i == 0:
-                return False
-    else:
+    if n <= 1:
         return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
     return True
 
 a, b = int(input()), int(input())
