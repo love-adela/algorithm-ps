e0ea279ecd81b7849c00dc7ad252ad1ae67a72c2
love-adelar <love.adelar@gmail.com>
2019-11-26 17:50:07 +0900

    range(2, n) 돌지 않고 range(2, int(n**0.5)+1) 돌게 함

--- acmicpc/1978/1978.py
+++ acmicpc/1978/1978.py
@@ -1,8 +1,9 @@
+import math
 # TODO: Sieve of Eratosthenes를 활용해서 미리 충분히 큰 수에 대해서는 채를 돌아서 미리 소수를 다 구해놓고 거기 있는지 확인해보자.
 
 def is_prime(n:int)->bool:
     if n != 1:
-        for i in range(2, n):
+        for i in range(2, int(n**0.5)+1):
             if n % i == 0:
                 return False
     else:
