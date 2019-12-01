2c1ae40d3c6eb2b806d4e0b44c622fcd4057455b
love-adelar <love.adelar@gmail.com>
2019-12-01 14:31:29 +0900

    A, B, C, D 점수 메김

---
+++ acmicpc/9498/9498.py
@@ -0,0 +1,12 @@
+grade = int(input())
+
+if 90<=grade<=100:
+    print('A')
+elif 80<=grade<90:
+    print('B')
+elif 70<=grade<80:
+    print('C')
+elif 60<=grade<70:
+    print('D')
+else:
+    print('F')
