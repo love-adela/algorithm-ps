26e96bed2a253fe7dafcf2f141eb6c159be8d0b0
love-adelar <love.adelar@gmail.com>
2019-12-05 21:25:15 +0900

    BFS로 수정해서 8가지 경우 나누어서 다시 풀었음

--- acmicpc/12761/12761.py
+++ acmicpc/12761/12761.py
@@ -1,13 +1,41 @@
+max_num = 100001
+visited = [False] * max_num 
+# queue = [정점번호, 해당 정점 번호까지의 거리]
+queue = []
 A, B, N, M = map(int, input().split())
-count = 0
-while M:
-    count += 1
-    if A < B:
-        if M % B != 0:
-            M -= A
-            continue
-        else:
-            M //= B
-            continue
-print(count)
-    
+
+queue.append((N, 0))
+visited[N] = True
+
+while len(queue) != 0:
+    current = queue[0][0]
+    count = queue[0][1]
+    del queue[0]
+
+    if current == M:
+        print(count)
+
+    if current + 1 < max_num and not visited[current+1]:
+        queue.append((current+1, count +1))
+        visited[current + 1] = True
+    if 0<current - 1 < max_num and not visited[current-1]:
+        queue.append((current-1, count +1))
+        visited[current-1] = True
+    if current + A < max_num and not visited[current+A]:
+        queue.append((current+A, count +1))
+        visited[current + A] = True
+    if 0<current - A < max_num and not visited[current-A]:
+        queue.append((current-A, count +1))
+        visited[current -A] = True
+    if current + B < max_num and not visited[current+B]:
+        queue.append((current+B, count +1))
+        visited[current+B] = True
+    if 0<current -B < max_num and not visited[current-B]:
+        queue.append((current-B, count +1))
+        visited[current-B] = True
+    if 0<current*A < max_num and not visited[current*A]:
+        queue.append((current*A, count +1))
+        visited[current*A] = True
+    if 0< current*B < max_num and not visited[current*B]:
+        queue.append((current*B, count +1))
+        visited[current*B] = True
