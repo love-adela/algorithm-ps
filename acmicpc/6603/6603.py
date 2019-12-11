0d90af1a63448842ce91052386f0cd0c2cf2640a
love-adelar <love.adelar@gmail.com>
2019-12-11 21:20:49 +0900

    itertools의 combinations 사용해서 수형도 그림

---
+++ acmicpc/6603/6603.py
@@ -0,0 +1,13 @@
+import sys
+from itertools import combinations
+
+flag = True
+while flag:
+    elem = list(map(int, sys.stdin.readline().strip().split()))
+    e = int(elem[0])
+    if e == 0:
+        flag = False
+        break
+    for i in combinations(elem[1:], 6):
+        print(' '.join(map(str, i)))
+    print()
