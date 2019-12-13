99f0bebaa82578477f50fd8a166ba11bc13b30c4
love-adelar <love.adelar@gmail.com>
2019-12-13 16:56:39 +0900

    0과 1로 이뤄진 입력값 총합의 두배가 전체 케이스 수보다 많으면 귀여운 걸로 계산

--- acmicpc/10886/10886.py
+++ acmicpc/10886/10886.py
@@ -1,12 +1,6 @@
 N = int(input())
-cnt_cute, cnt_not_cute = 0, 0 
-
-while N:
-    if int(input()) == 1:
-        cnt_cute += 1
-    else:
-        cnt_not_cute += 1
-    N -=1
-if cnt_cute > cnt_not_cute: print('Junhee is cute!')
+count = 0
+for _ in range(N):
+    count += int(input())
+if count *2 > N: print('Junhee is cute!')
 else: print('Junhee is not cute!')
-
