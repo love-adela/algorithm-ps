00f712b72655b66da8ac3ec07e96a7686838f3ef
love-adelar <love.adelar@gmail.com>
2019-12-18 18:26:24 +0900

    주석 추가

--- acmicpc/3944/3944.py
+++ acmicpc/3944/3944.py
@@ -3,3 +3,4 @@ read = sys.stdin.readline
 for _ in range(int(read())):
     B,D=read().split()
     print(sum(map(int,D))%(int(B)-1))
+    # D를 바로 Integer casting해버리면 'int' object는 iterable 하지 않기 때문에 sum연산이 되지 않는다. 
