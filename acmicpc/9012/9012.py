0538a7b84428d35da939bf927e940a7fd9ff9eba
love-adelar <love.adelar@gmail.com>
2020-01-07 02:25:20 +0900

    Stack 이용해서 괄호 매칭되는지 검사

---
+++ acmicpc/9012/9012.py
@@ -0,0 +1,28 @@
+import sys
+T = int(input())
+
+def is_vps(string:str)->str:
+    characters = [param for param in string]
+    index = 0
+    
+    while len(characters) != 0:
+    #    print(characters)
+        if index < 0:
+            break
+        c = characters.pop()
+
+        if c == '(':
+            index -= 1
+        elif c == ')':
+            index += 1
+
+    if index == 0:
+        return 'YES' 
+    else:
+        return 'NO'
+
+while T:
+    parentheses = sys.stdin.readline()
+    print(is_vps(parentheses))
+    T -=1
+
