370498fa370511b61ee01ed8db0f1b702750de03
love-adelar <love.adelar@gmail.com>
2020-01-13 00:17:25 +0900

    조합 결과에 0이 나타나려면 2와 5가 나타나는 경우의 수를 구해 최솟값 찾아냄

--- acmicpc/2004/2004.py
+++ acmicpc/2004/2004.py
@@ -1,49 +1,47 @@
 import sys
 n, m = map(int, sys.stdin.readline().split())
 
-if n == 0 and m == 0:
-    print(0)
-else:
-# n!에 있는 2의 개수
-# 예를 들면 100!에 5가 몇 개인지 세려면
-# 100/5=20, 20/5=4, 4/5=0 이렇게 해서 20+4를 하면 됨
-
-
+    
+def get_two(n, m):
+    # n!에 있는 2의 개수
+    k = n-m
     count_2 = 0
     while n > 0:
         n //= 2 
         count_2 += n
-    print(count_2)
     
-# n!에 있는 5의 개수
+    # m!에 있는 2의 개수
+    while m > 0:
+        m //= 2
+        count_2 -= m
+
+    # (n-m)!에 있는 2의 개수
+    while k > 0:
+        k //= 2
+        count_2 -= k
+    return count_2
+
+def get_five(n, m):
+    # n!에 있는 5의 개수
+    k = n-m
     count_5 = 0
     while n > 0:
         n //= 5
         count_5 += n
-    print(count_5)
 
-# m!에 있는 2의 개수
+    # m!에 있는 5의 개수
     while m > 0:
-        m //= 2
-        count_2 -= m
-    print(count_2)
-
-# m!에 있는 5의 개수
-    while m > 0:
-        m //=2
+        m //=5
         count_5 -= m
-    print(count_5)
 
-# (n-m)!에 있는 2의 개수
-    k = n-m
+    # (n-m)!에 있는 5의 개수
     while k > 0:
-        k //= 2
-        count_2 -= k
-    print(count_2)
-# (n-m)!에 있는 5의 개수
-    while k > 0:
-        k //= 2
+        k //=5
         count_5 -= k
-    print(count_5)
 
+    return count_5
 
+if n == 0 and m == 0:
+    print(0)
+else:
+    print(min(get_two(n, m), get_five(n, m)))
