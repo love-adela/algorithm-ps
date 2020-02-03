df04319725ef99372a37422a41f0d81a50c57eb9
love-adelar <love.adelar@gmail.com>
2020-02-03 23:40:56 +0900

    함수로 구현하지 않는 방법

--- acmicpc/1783/1783.py
+++ acmicpc/1783/1783.py
@@ -1,14 +1,13 @@
 N, M = map(int, input().split())
+count = 0
 
-def main():
-    if N == 1:
-        return 1
-    elif N == 2:
-        return min(4, (M+1)//2)
-    elif N >= 3:
-        if M >= 7:
-            return M -2
-        else:
-            return min(4, M)
+if N is 1:
+    count = 1
+elif N is 2:
+    count = min(4, (M+1)//2)
+elif M < 7:
+    count = min(M, 4)
+else:
+    count =M -2 # 5 + m -7
 
-print(main())
+print(count)
