717e806921aa6beaf9247b70563f9615f131bb6b
love-adelar <love.adelar@gmail.com>
2020-02-07 03:15:08 +0900

    2의 거듭제곱꼴인지 확인

---
+++ acmicpc/11966/11966.py
@@ -0,0 +1,3 @@
+n = int(input())
+matrix = [2**i for i in range(31)]
+print(1) if n in matrix else print(0)
---
+++ acmicpc/11966/random-generator.py
@@ -0,0 +1,5 @@
+from random import randint
+N=100
+while N:
+    print(randint(1, 1073741824))
+    N -=1
