8b820474f2adf086d22c943ac479c6c1117e3bb7
love-adelar <love.adelar@gmail.com>
2020-02-08 16:40:09 +0900

    2를 포함하는 조건 포함

--- acmicpc/11896/11896.py
+++ acmicpc/11896/11896.py
@@ -1,9 +1,12 @@
-import sys
-a, b = map(int, sys.stdin.readline().split())
 # a와 b사이에 있는 4 이상 짝수의 합
 # 등차수열의 합을 이용
-count = 0
-for i in range(a, b+1):
-    if i >= 4 and i % 2 == 0:
-        count += i
-print(count)
+
+import sys
+a, b = map(int, sys.stdin.readline().split())
+if a % 2 != 0: a += 1
+if b % 2 != 0: b -= 1
+total = ((a + b) //2)*(b//2-a//2+1)
+if a <= 2 and b >=2:
+    print(total-2)
+else:
+    print(total)
