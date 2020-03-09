6de06b1d3bc60c49ae2d6c4d7835ff39aaade52e
love-adelar <love.adelar@gmail.com>
2020-03-10 02:03:57 +0900

    This algorithm is a Bubble Sort

---
+++ acmicpc/2947/2947.py
@@ -0,0 +1,6 @@
+numbers = list(map(int, input().split()))
+for i in range(len(numbers)):
+    for j in range(len(numbers) - (1+i)):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+            print(' '.join(str(num) for num in numbers))
