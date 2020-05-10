76bc9584e261701274dd950637d906baf1426f48
love-adelar <love.adelar@gmail.com>
2020-05-10 16:37:39 +0900

    file directory변경

--- acmicpc/17626/17626.py
+++ acmicpc/17626/17626.py
@@ -1,5 +1,20 @@
 n = int(input())
-def main(k):
-    pass
 
-# DP: 일반항 구해보자
+fs = [i*i for i in range(224)]
+dp = [4 for x in range(n+1)]
+visit = [0 for x in range(n+1)]
+dp[0] = 0
+dp[1] = 1
+
+exNum = 50000**2
+for i in range(1, n+1):
+    minNum = exNum
+    j = 1
+    while(not visit[i] and fs[j] <= i):
+        minNum = min(dp[i-fs[j]], minNum)
+        j = j+1
+    visit[i] = 1
+    dp[i] = minNum+1
+
+print(dp[n])
+
--- acmicpc/2526/2526.py
+++ acmicpc/2526/2526.py
@@ -4,6 +4,9 @@ order = dict()
 while True:
     M = M * N % P
     if M in order:
+        print(f'M: {M}')
+        print(order)
+        print(order[M])
         print(len(order)-order[M])
         break
     else:
