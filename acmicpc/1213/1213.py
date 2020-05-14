4a051bd39e539b0fe360c2a63efefd0958951baa
love-adelar <love.adelar@gmail.com>
2020-05-14 23:04:21 +0900

    팰린드롬 만들기 문제(Brute force) 틀림. 다시 풀어야 함

---
+++ acmicpc/1213/1213.py
@@ -0,0 +1,10 @@
+from itertools import permutations, combinations
+
+def is_palin(name:str)->bool:
+    name
+    for e in lst:
+        if e == e[::-1]:
+            return True
+    return False 
+s = input()
+print(is_palin(s))
