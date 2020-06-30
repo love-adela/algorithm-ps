4eb8b42d3fc3153ea9c67ffca17594d8009b16ae
love-adelar <love.adelar@gmail.com>
2020-06-30 17:29:07 +0900

    문제 특징 주석으로 달아놓음

--- acmicpc/1012/1012-1.py
+++ acmicpc/1012/1012-1.py
@@ -10,6 +10,7 @@ def dfs(curr_x, curr_y, visited):
     for dx, dy in (-1, 0), (0, -1), (1, 0), (0, 1):
         next_x = curr_x + dx
         next_y = curr_y + dy
+        # 무한루프가 돌지 않게 처리 : 옆에 있는 정점끼리 왔다갔다 하는걸 막음
         if 0<= next_x<M and 0<=next_y< N and not visited[next_x][next_y]:
             if table[next_x][next_y] == 1:
                 res = dfs(next_x, next_y, visited)
@@ -28,6 +29,8 @@ while T:
         table[Y][X] = 1
     for i in range(M):
         for j in range(N):
+            # 연결요소가 여러개이기 때문에 visited 체크를 해줘야 함
+            # 다녀왔던 정점도 dfs가 가능하다고 여기지 않게 막음
             if table[i][j] == 1 and not visited[i][j]:
                 dfs(i, j, visited)
                 answer += 1
