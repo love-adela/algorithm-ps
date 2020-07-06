6e2cbbd32189054f7d100a8928c0d48220eb8502
love-adelar <love.adelar@gmail.com>
2020-07-07 02:05:26 +0900

    백트랙킹 기본, 1 8 과 8 1 을 구분하지 않는 문제

---
+++ acmicpc/15654/15654.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+
+def get_seq(included, length):
+    if length == M:
+        print(' '.join(list(map(str, included))))
+    else:
+        for item in sequence:
+            if item not in included:
+                get_seq(included+[item], length+1)
+
+sequence = sorted([int(param) for param in input().split()])
+get_seq([], 0)
