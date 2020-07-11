5f49ca4d60caa3d4919b3cbb1a43b3ad2eaa0ef0
love-adelar <love.adelar@gmail.com>
2020-07-11 20:56:26 +0900

    try - catch를 if로 바꿔줌

--- acmicpc/2910/2910.py
+++ acmicpc/2910/2910.py
@@ -3,10 +3,10 @@ sequence = list(map(int, input().split()))
 frequency = dict() # key: 숫자, value : 빈도
 
 for number in sequence:
-    try: 
+    if number in frequency:
         params = frequency[number]
         frequency[number]+=1
-    except:
+    else:
         frequency[number] = 1
 
 params = sorted(frequency.items(), key = lambda x:x[1], reverse=True)
