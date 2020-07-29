482a0e210d09529446679ba1b4c908449f74335d
love-adelar <love.adelar@gmail.com>
2020-07-29 22:59:06 +0900

    코드를 친절하게 수정했음. - 각 변수에 대한 설명 적어둠 - list, set comprehension으로 고침

--- acmicpc/1235/1235.py
+++ acmicpc/1235/1235.py
@@ -1,15 +1,15 @@
+"""
+j: 각 문자열에서 남겨놓을 맨 뒤 글자 수
+seq: 끝 j글자만 남겼을 때 중복을 제외한 문자열 목록
+n: 전체 문자열 개수
+"""
+
 N = int(input())
-students = []
-for _ in range(N):
-    student = input()
-    students.append(student)
+students = [input() for _ in range(N)]
 length = len(students[0])
 
 for j in range(1, length+1):
-    seq = set()
-    for i in range(N):
-        item = students[i][-j:]
-        seq.add(item)
+    seq = {student[-j:] for student in students}
     if len(seq) == N:
         print(j)
         break
