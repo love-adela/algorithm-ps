84462fafb06ccf96b7737ab005757aabbf09d4c9
love-adelar <love.adelar@gmail.com>
2020-07-30 20:49:08 +0900

    List comprehension으로 풀음

--- acmicpc/1251/1251.py
+++ acmicpc/1251/1251.py
@@ -1,8 +1,4 @@
 from itertools import combinations
-word = input()
-lst = []
-for item in combinations(range(1, len(word)), 2):
-    item = word[:item[0]][::-1] + word[item[0]: item[1]][::-1] + word[item[1]:][::-1]
-    lst.append(item)
-print(sorted(lst)[0])
+s = input()
+print(sorted([s[:com[0]][::-1]+s[com[0]:com[1]][::-1]+s[com[1]:][::-1] for com in combinations(range(1, len(s)), 2)])[0])
 
