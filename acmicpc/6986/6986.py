44102ab7a863d3a367c2d408853965a24a6b1cec
love-adelar <love.adelar@gmail.com>
2020-08-06 19:15:39 +0900

    디버깅 오래 걸림. 부동소수점 오차 처리하는 방법 익힘

---
+++ acmicpc/6986/6986.py
@@ -0,0 +1,18 @@
+# boj.kr/6986 절사평균
+import sys
+read = lambda: sys.stdin.readline()
+N, K = map(int, input().split())
+data = sorted([float(read())*10 for _ in range(N)])
+trimmed_data = data[K:N-K]
+
+def get_trimmed_mean():
+    answer = sum(trimmed_data) / (N-K*2)
+    return '{:.2f}'.format(round(answer*0.1, 2))
+
+def get_adjusted_mean():
+    adjusted_data = [trimmed_data[0]] * K + trimmed_data + [trimmed_data[-1]] * K
+    answer = sum(adjusted_data) / N
+    return '{:.2f}'.format(round(answer*0.1, 2))
+
+print(get_trimmed_mean())
+print(get_adjusted_mean())
