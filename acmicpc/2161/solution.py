4b00d0c97eabf0275f7aebda867747a5f0025f98
love-adelar <love.adelar@gmail.com>
2020-08-15 23:21:05 +0900

    재귀함수 사용하지 않고 코드 깔끔하게 바꿈~

--- acmicpc/2161/solution.py
+++ acmicpc/2161/solution.py
@@ -1,26 +1,17 @@
-from sys import setrecursionlimit
-
-"""
+'''
 1. N번 동안 맨 위에 있는 카드 버리고 출력하기
 2. 제일 위의 카드 맨 밑으로 옮기기
-"""
+'''
 
-def move_top_to_bottom(num_lst:list)->list:
-    return num_lst[1:] + [num_lst[0]]
-
-def discard_top_card(num_lst:list)->list:
-    return num_lst[1:]
-
-
-def main(numbers):
-    print(numbers[0], end=' ')
-    if len(numbers) == 1:
-        return 
-    new_numbers = discard_top_card(numbers)
-    final_numbers = move_top_to_bottom(new_numbers)
-    return main(final_numbers)
-
-setrecursionlimit(1010)
 N = int(input())
-numbers = [int(num) for num in range(1, N+1)]
-main(numbers)
+numbers = [num+1 for num in range(N)]
+while True:
+    # 맨 위에 있는 카드를 한장 뽑아 출력
+    pick, *numbers = numbers
+    print(pick, end=' ')
+
+    if len(numbers) == 0:
+        break
+
+    # 맨 위에 있는 카드 한장을 뽑아 맨 밑으로 넣음
+    numbers = numbers[1:] + [numbers[0]]
