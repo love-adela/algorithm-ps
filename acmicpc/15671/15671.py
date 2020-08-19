cbbeadbd417766e27b69eb9ff85e810b8b4395b1
love-adelar <love.adelar@gmail.com>
2020-08-19 23:25:12 +0900

    상태 변화를 추상화/단순화하여 컨테이너의 값 중 하나를 고정 후 그에 대해 순회

--- acmicpc/15671/15671.py
+++ acmicpc/15671/15671.py
@@ -1,41 +1,56 @@
+# boj.kr/15671
 import sys
-sys.setrecursionlimit(1000)
-N = int(input())
-grid = [['.' for _ in range(N+1)] for _ in range(N+1)]
+read = lambda: sys.stdin.readline()
+N = int(read())
+grid = [['.' for _ in range(7)] for _ in range(7)]
 grid[3][3], grid[4][4] = 'W', 'W'
 grid[3][4], grid[4][3] = 'B', 'B'
 
 dx = [-1, 0, 1, 1, 1, 0, -1, -1]
 dy = [1, 1, 1, 0, -1, -1, -1, 0]
 
-def is_captured(cur_x, cur_y, nxt_x, nxt_y, d)->bool:
-    if not (0<nxt_x<=6 and 0<nxt_y<=6) or grid[nxt_x][nxt_y] == '.':
-        return False
-    if grid[nxt_x][nxt_y] == grid[cur_x][cur_y]:
-        return True
-    return is_captured(cur_x, cur_y, nxt_x+dx[d], nxt_y+dy[d], d)
-
-def change_color(cur_x, cur_y, d): 
+def is_captured(cur_x, cur_y, d)->bool:
     nxt_x = cur_x + dx[d]
     nxt_y = cur_y + dy[d]
-    if grid[cur_x][cur_y] != grid[nxt_x][nxt_y]:
+    while True: 
+        # 이번 nxt_x, nxt_y에서 수행할 동작(nxt_x, nxt_y는 고정)
+        if not (0<nxt_x<=6 and 0<nxt_y<=6) or grid[nxt_x][nxt_y] == '.':
+            return False
+        if grid[nxt_x][nxt_y] == grid[cur_x][cur_y]:
+            return True
+        # 이번 nxt_x, nxt_y에서 해결이 안돼서 다음 칸으로 이동
+        nxt_x += dx[d]
+        nxt_y += dy[d]
+
+def change_color(cur_x, cur_y, d): 
+    """
+    * (x, y) 위치에 돌을 놓았을 때, d 방향으로 제일 가까운 같은 색의 돌 사이의 모든 돌 뒤집기
+    * (x, y) 위치에 돌을 놓았을 때, d 방향으로 같은 색의 돌이 나올 때까지 모든 돌을 뒤집기
+    """
+    nxt_x = cur_x + dx[d]
+    nxt_y = cur_y + dy[d]
+
+    while grid[cur_x][cur_y] != grid[nxt_x][nxt_y]:
+        # 다른색일 때
         grid[nxt_x][nxt_y] = grid[cur_x][cur_y]
-    return
+        nxt_x += dx[d]
+        nxt_y += dy[d]
 
 for i in range(N):
-    x, y = map(int, input().split())
-    if i % 2 == 0:
+    x, y = map(int, read().split()) # for 돌 놓은 위치(고정) in 돌 로그
+    if i % 2 == 0: # 돌 놓은 위치에 돌 놓기
         grid[x][y] = 'B'
     else:
         grid[x][y] = 'W'
-    for j in range(8):
-        if is_captured(x, y, x+dx[j], y+dy[j], j) :
-            change_color(x, y, j)
+    # 돌 놓은 위치 주변 8방향을 전부 확인하면서 같은 색 사이에 있는 돌 모두 뒤집기
+    for j in range(8): # for 방향 in 주변 8방향
+        if is_captured(x, y, j) : # for 돌 in 그 방향으로 제일 가까운 같은 색 사이에 있는 돌 목록
+            change_color(x, y, j) # 돌 뒤집어주기
 
 cnt_w, cnt_b = 0, 0
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
+for i in range(1, 7):
+    for j in range(1, 7):
         if grid[i][j] == 'W':
             print('W', end='')
             cnt_w += 1
@@ -44,6 +59,6 @@ for i in range(1, N+1):
             cnt_b += 1
         else:
             print('.', end='')
+    print()
 
-if cnt_w > cnt_b: print('White')
-else: print('Black')
+print('Black' if cnt_b > cnt_w else 'White')
