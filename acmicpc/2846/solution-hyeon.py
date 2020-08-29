1a6b2c6b3e87310316993869b6de5999ba74a431
love-adelar <love.adelar@gmail.com>
2020-08-29 19:33:11 +0900

    조금 더 간결하게 수정

--- acmicpc/2846/solution-hyeon.py
+++ acmicpc/2846/solution-hyeon.py
@@ -1,16 +1,18 @@
 N = int(input())
 heights = [int(param) for param in input().split()]
 
-current_sub = [] 
-last = 0
+current_sub = [heights[0]] 
+last = heights[0]
 maximum = 0
-for item in heights:
+
+for item in heights[1:]:
     if item <= last:
         diff = current_sub[-1] - current_sub[0]
         maximum = max(maximum, diff)
         current_sub = []
     current_sub.append(item)
     last = item
+
 diff = current_sub[-1] - current_sub[0]
 maximum = max(maximum, diff)
 print(maximum)
