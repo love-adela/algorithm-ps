6d367f4b24f27b3f687aaf64a7fd0fe3effd070b
love-adelar <love.adelar@gmail.com>
2020-08-29 20:52:33 +0900

    cpp 파일 실행 스크립트 추가

--- acmicpc/2846/test.sh
+++ acmicpc/2846/test.sh
@@ -1,3 +1,14 @@
+echo 'Python version:'
 python3 solution-hyeon.py < input0
 python3 solution-hyeon.py < input1
 python3 solution-hyeon.py < input2
+echo
+echo 'C version:'
+./a.out < input0
+./a.out < input1
+./a.out < input2
+echo
+echo 'C++ version:'
+./solution < input0
+./solution < input1
+./solution < input2
