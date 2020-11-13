89b3dc532f85cb28ba1970a1e4a9420f44927d86
love-adelar <love.adelar@gmail.com>
2020-11-14 00:04:53 +0900

    프로그래머스 스킬트리 문제 풂

---
+++ programmers/skill-test-lv2/solution-2.py
@@ -0,0 +1,21 @@
+def is_validate(item:str, string:str)->bool:
+    chr_list = [c for c in item if c in string]
+    check_list = []
+    for i in range(len(chr_list)):
+        if string[0] in chr_list and chr_list[i] == string[i]:
+            check_list.append(chr_list[i])
+        else:
+            return False
+    if check_list in chr_list or check_list == chr_list:
+        return True
+    else:
+        return False
+
+skill = 'CBD'
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+cnt = 0
+for tree in skill_trees:
+    if is_validate(tree, skill) == True:
+        cnt += 1
+
+print(cnt)
