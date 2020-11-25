a8e0f136db757f7a68ebd0644fcaf0b034258562
love-adelar <love.adelar@gmail.com>
2020-11-25 22:38:30 +0900

    MISC: 쓸모 없는 주석 삭제

--- programmers/skill-test-lv1/crane_dolls.py
+++ programmers/skill-test-lv1/crane_dolls.py
@@ -6,7 +6,7 @@ def get_top(board, move):
             board[i][move-1] = 0
             return res
 
-## Test
+# Test
 #board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 #moves = [1,5,3,5,1,2,1,4]
 
