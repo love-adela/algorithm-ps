3de3c33b955199a855f2421c0a8ccadddf9232f6
love-adelar <love.adelar@gmail.com>
2020-11-25 22:28:20 +0900

    Refactoring code

---
+++ programmers/skill-test-lv1/crane_dolls_simple.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    stack = []
+    cnt = 0
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move -1] != 0:
+                res = board[i][move-1]
+                board[i][move-1] = 0
+                if len(stack) == 0:
+                    stack.append(res)
+                elif len(stack) > 0 and res != stack[-1]:
+                    stack.append(res)
+                else:
+                    cnt += 1
+                    del stack[-1]
+                break
+    return cnt *2
