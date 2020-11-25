6975018c9782931619be55841f2da9279c03b6ff
love-adelar <love.adelar@gmail.com>
2020-11-25 17:02:50 +0900

    문제 틀린 이유 적어놓음

--- programmers/skill-test-lv1/crane_dolls.py
+++ programmers/skill-test-lv1/crane_dolls.py
@@ -10,14 +10,20 @@ def get_top(board, move):
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
 
-# result = 4
 stack = []
 for move in moves:
     stack.append(get_top(board, move))
 
 # Remove None
-new_stack = [item for item in stack if item]
+# new_stack = [item for item in stack if item]
+new_stack = [1, 4, 2, 2, 4, 1, 1]
 
+"""
+이 풀이의 문제점
+- 맨 위에 겹친 두개 아이템씩 없애야 함
+- 맨 앞부터 겹쳐진 아이템 찾아서 겹친게 3개 이상 생길 수 있음
+    - 예시: [1, 4, 2, 2, 4, 1, 1]
+"""
 def get_radix(stack:list):
     for i in range(len(stack)-1):
         if stack[i] == stack[i+1]:
@@ -28,6 +34,7 @@ def remove_radix_values(new_stack):
     if not radix:
         return new_stack
     new_stack = [item for item in new_stack if item != radix]
+    print(new_stack)
     return remove_radix_values(new_stack)
 
 res_stack = remove_radix_values(new_stack)
